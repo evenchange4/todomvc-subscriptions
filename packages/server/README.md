c69447b2501e9dc7d8b8dd35d84feb06186f190c
Michael Hsu (徐承志) <evenchange4@gmail.com>
2017-12-27 17:09:15 +0800

    bug > Error! Unexpected character '�' (1:0)   /Users/.../node_modules/apollo-engine-binary-darwin/engineproxy_darwin_amd64

--- packages/server/README.md
+++ packages/server/README.md
@@ -10,18 +10,20 @@ $ npm run dev
 ```cmd
 # Option 1: Nodejs
 $ npm run build
-$ npm start
+$ PORT=4000 PROXY_PORT=4001 APOLLO_ENGINE_KEY={key} npm start
 
 # Option 2: Binary
 $ npm install sqlite3 --save
 $ npm run pkg
-$ PORT=4000 ./server
+$ PORT=4000 PROXY_PORT=4001 APOLLO_ENGINE_KEY={key} ./server
 
 # Option 3: Docker
 $ docker build -t todomvc-server .
 $ docker run --rm -it \
-  -p 4000:4000 \
+  -p 4001:4001 \
   -e "PORT=4000" \
+  -e "PROXY_PORT=4001" \
+  -e "APOLLO_ENGINE_KEY={key}"
   todomvc-server
 ```
 
--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -5,9 +5,11 @@ require('dotenv').config();
 import { GraphQLServer, PubSub } from 'graphql-yoga';
 import * as R from 'ramda';
 import CLIENT_ID from '@todomvc-subscriptions/constants/clientID';
+import { Engine } from 'apollo-engine';
+import compression from 'compression';
 import { typeDefs, resolvers, models, connector } from './todos';
 
-const { PORT } = process.env;
+const { PORT, PROXY_PORT, APOLLO_ENGINE_KEY } = process.env;
 
 async function run() {
   try {
@@ -26,8 +28,39 @@ async function run() {
       options: {
         port: PORT,
         disablePlayground: false,
+        tracing: true,
       },
     });
+
+    /**
+     * Apollo Engine
+     * ref: https://github.com/graphcool/graphql-yoga/issues/5#issuecomment-351695388
+     */
+    const engine = new Engine({
+      engineConfig: {
+        apiKey: APOLLO_ENGINE_KEY,
+        logging: {
+          level: 'INFO',
+        },
+        origins: [
+          {
+            http: {
+              url: `http://localhost:${PORT}`,
+            },
+          },
+        ],
+        frontends: [
+          {
+            host: '0.0.0.0',
+            port: parseInt(PROXY_PORT, 10),
+            endpoint: '/',
+          },
+        ],
+      },
+    });
+    engine.start();
+    server.express.use(compression());
+    server.express.use(engine.expressMiddleware());
 
     await server.start();
   } catch (error) {
@@ -36,4 +69,6 @@ async function run() {
   }
 }
 
-run().then(() => console.log(`Server is running on http://localhost:${PORT}`)); // eslint-disable-line
+run().then(() =>
+  console.log(`Proxy Server is running on http://localhost:${PROXY_PORT}`),
+); // eslint-disable-line
